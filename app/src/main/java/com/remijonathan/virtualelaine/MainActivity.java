837997732fded9864659990d360e6d2dda5107f2
RemiJonathan <contact@remijonathan.com>
2019-11-22 22:32:58 -0500

    Setup Create Item Button

--- app/src/main/java/com/remijonathan/virtualelaine/MainActivity.java
+++ app/src/main/java/com/remijonathan/virtualelaine/MainActivity.java
@@ -1,8 +1,13 @@
 package com.remijonathan.virtualelaine;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    /*Handling a Menu item
+     * REMINDERS
+     * return true; not super
+     * */
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.new_task_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.create_menu_item:
+                Toast.makeText(this, "Create new Task", Toast.LENGTH_LONG).show();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
