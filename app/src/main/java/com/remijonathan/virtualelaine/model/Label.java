0df98a4ddfc64e321e1d05e9eaa09cf2efb81100
RemiJonathan <contact@remijonathan.com>
2019-11-28 20:20:38 -0500

    Added Label Model

---
+++ app/src/main/java/com/remijonathan/virtualelaine/model/Label.java
@@ -0,0 +1,40 @@
+package com.remijonathan.virtualelaine.model;
+
+public class Label {
+    private int id;
+    private String title;
+    private int color;
+    private String description;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getColor() {
+        return color;
+    }
+
+    public void setColor(int color) {
+        this.color = color;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
