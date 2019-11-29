f2a765d22199ba218483e2b77befff47fda5a3ef
RemiJonathan <contact@remijonathan.com>
2019-11-28 19:18:09 -0500

    Created DatePicker Fragment

--- app/src/main/java/com/remijonathan/virtualelaine/CreateNewTaskActivity.java
+++ app/src/main/java/com/remijonathan/virtualelaine/CreateNewTaskActivity.java
@@ -1,8 +1,13 @@
 package com.remijonathan.virtualelaine;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.DialogFragment;
 
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.icu.util.Calendar;
 import android.os.Bundle;
+import android.widget.DatePicker;
 
 public class CreateNewTaskActivity extends AppCompatActivity {
 
