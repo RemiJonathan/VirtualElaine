d4cacaec612ac8e749a516697735fd4f253b122a
RemiJonathan <contact@remijonathan.com>
2019-12-01 23:08:19 -0500

    Added select Label Dialog

--- app/src/main/java/com/remijonathan/virtualelaine/CreateNewTaskActivity.java
+++ app/src/main/java/com/remijonathan/virtualelaine/CreateNewTaskActivity.java
@@ -2,18 +2,24 @@ package com.remijonathan.virtualelaine;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.DialogFragment;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
 import android.app.DatePickerDialog;
 import android.app.TimePickerDialog;
 import android.icu.util.Calendar;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.TimePicker;
 import android.widget.Toast;
 
 import com.google.android.material.chip.Chip;
+import com.google.gson.Gson;
+import com.remijonathan.virtualelaine.data.DatabaseHelper;
 import com.remijonathan.virtualelaine.model.Label;
 
 import java.util.List;
@@ -22,13 +28,16 @@ public class CreateNewTaskActivity extends AppCompatActivity implements TimePick
     private EditText titleEditText;
     private Chip dateTimeSelect;
     private Calendar dueDateAndTime = Calendar.getInstance();
-    private List<Label> labels;
+    private Chip labelSelect;
+
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_new_task);
+
+        titleEditText = findViewById(R.id.title_edit_text);
 
         dateTimeSelect = findViewById(R.id.chip_dateTime);
         dateTimeSelect.setOnClickListener(new View.OnClickListener() {
@@ -38,6 +47,15 @@ public class CreateNewTaskActivity extends AppCompatActivity implements TimePick
                 timePicker.show(getSupportFragmentManager(), "time picker");
             }
         });
+
+        labelSelect = findViewById(R.id.chip_label);
+        labelSelect.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showLabelSelectDialog();
+            }
+        });
+
     }
 
     @Override
@@ -63,5 +81,16 @@ public class CreateNewTaskActivity extends AppCompatActivity implements TimePick
         else timeToDate = timeBetween + " day";
 
         dateTimeSelect.setText(String.format("%s (%s)", dueDate, timeToDate));
+
+        //TODO: send the time somewhere
+        Gson gson = new Gson();
+
+        String DateTimeDueJson = gson.toJson(dueDateAndTime);
+    }
+
+    public void showLabelSelectDialog(){
+        DialogFragment selectLabelFragment = new SelectLabelFragment();
+        selectLabelFragment.show(getSupportFragmentManager(),"label picker");
+
     }
 }
